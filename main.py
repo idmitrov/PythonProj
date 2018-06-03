4e134599170f9cf24f2db588ec1cf03be590aebc
BiserVStoev <biser.v.stoev@gmail.com>
2018-06-03 13:51:01 +0300

    test

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+cwd = os.getcwd()
+import sys
+sys.path.insert(0, cwd + '/src')
+import writter
+import reader
+import searcher
+
+# writter.test()
+print(cwd)
+termCount = input("Enter the number of terms you are going to search for:")
+print(termCount)
+for x in range(1, termCount + 1):
+    print(x)    
+  # Python 3
 #import reader, writer, searcher
 
 # try
